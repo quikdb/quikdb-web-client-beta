0070d5f0bc9e4a70c1aaf5f54568d6a2f024835a
chizobavictory <chizoba.victory1@gmail.com>
2024-11-27 14:36:17 +0100

    added auth pages

--- apps/app/app/(unauthenticated)/authCode/page.tsx
+++ apps/app/app/(unauthenticated)/authCode/page.tsx
@@ -2,8 +2,8 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import { Button } from '../../../components/ui/button';
-import { Input, FormDivider, PasswordInput, FormHeader } from '../../../components/onboarding';
+import { Button } from '../../components/ui/button';
+import { Input, FormDivider, PasswordInput, FormHeader } from '../../components/onboarding';
 
 const SignInPage = () => {
   const [seeOtherOptions, setSeeOtherOptions] = useState(false);
