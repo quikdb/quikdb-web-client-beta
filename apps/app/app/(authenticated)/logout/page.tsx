6eb2de0fc73c434ccf3bf5dda91fa3de6490e5a8
chizobavictory <chizoba.victory1@gmail.com>
2024-11-28 12:16:20 +0100

    added profect pages

---
+++ apps/app/app/(authenticated)/logout/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@repo/design-system/components/ui/button';
+import Link from 'next/link';
+
+const Onboarding = () => {
+  return (
+    <main className='min-h-screen px-20 py-7'>
+      <header>
+        <p className='text-gradient'>quikdb</p>
+      </header>
+      <div className='flex flex-col mt-20 gap-7'>
+        <Link href='/sign-up' className='container'>
+          <Button className='bg-gradient'>Go to Signup</Button>
+        </Link>
+        <Link href='/sign-in' className='container'>
+          <Button className='bg-gradient'>Go to Login</Button>
+        </Link>
+        <Link href='/organizations' className='container'>
+          <Button className='bg-gradient'>Go to Organizations</Button>
+        </Link>
+        <Link href='/' className='container'>
+          <Button className='bg-gradient'>Go to Dashboard</Button>
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default Onboarding;
