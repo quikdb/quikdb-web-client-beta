595ac9e96e72fdbf0dbbcd769ea63a60118560fb
chizobavictory <chizoba.victory1@gmail.com>
2024-11-28 21:53:18 +0100

    completed auth integrations

--- apps/app/app/(unauthenticated)/verify-otp/page.tsx
+++ apps/app/app/(unauthenticated)/verify-otp/page.tsx
@@ -1,38 +1,31 @@
 'use client';
+
 import React, { useState } from 'react';
 import { Input, FormHeader } from '@repo/design-system/components/onboarding';
 import { Button } from '@repo/design-system/components/ui/button';
 import axios from 'axios';
-import { CryptoUtils } from '@repo/design-system/lib/cryptoUtils'; // Adjust the import path as necessary
+import { CryptoUtils } from '@repo/design-system/lib/cryptoUtils';
 
 const VerifyOTP: React.FC = () => {
   const [email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
   const [otp, setOtp] = useState('');
-  const [error, setError] = useState(''); // Declare error state
-  const [success, setSuccess] = useState(false); // Declare success state
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     setLoading(true);
-    setError(''); // Reset error state
-    setSuccess(false); // Reset success state
-
+    setError('');
+    setSuccess(false);
+    
     try {
       const data = {
         email,
         OTPType: 'signup',
         otp,
       };
-
-      // Encrypt the data before sending
-      const encryptedData = CryptoUtils.aesEncrypt(
-        JSON.stringify(data),
-        'mysecurekey1234567890',
-        'uniqueiv12345678'
-      );
-
-      // Use Axios for the POST request
+      const encryptedData = CryptoUtils.aesEncrypt(JSON.stringify(data), 'mysecurekey1234567890', 'uniqueiv12345678');
       const response = await axios.post(
         'https://quikdb-core-beta.onrender.com/a/verifyOtp',
         {
@@ -44,11 +37,9 @@ const VerifyOTP: React.FC = () => {
           },
         }
       );
-
-      // Handle success or failure based on the response
       if (response.status === 200) {
-        setSuccess(true); // OTP verified successfully
-        setError(''); // Clear any previous errors
+        setSuccess(true);
+        setError('');
       } else {
         setError('Failed to verify OTP: ' + response.data.error);
       }
@@ -63,26 +54,11 @@ const VerifyOTP: React.FC = () => {
   return (
     <div className='flex flex-col w-full p-10'>
       <FormHeader title='Verify OTP' description={`Enter your One-time Password code sent to ${email || 'your email'}.`} showLogo />
-
       <main className='flex flex-col items-center justify-center my-16 w-full'>
         <div className='flex flex-col w-full md:w-[680px] items-center'>
           <form onSubmit={handleSubmit} className='flex flex-col gap-y-4 items-center w-full'>
-            <Input
-              type='email'
-              placeholder='Enter Email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-
-            <Input
-              type='text'
-              placeholder='Enter Code'
-              value={otp}
-              onChange={(e) => setOtp(e.target.value)}
-              required
-            />
-
+            <Input type='email' placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <Input type='text' placeholder='Enter Code' value={otp} onChange={(e) => setOtp(e.target.value)} required />
             <Button
               type='submit'
               disabled={loading}
@@ -90,10 +66,8 @@ const VerifyOTP: React.FC = () => {
             >
               {loading ? 'Verifying...' : 'Verify OTP'}
             </Button>
-
             {error && <p className='text-red-500'>{error}</p>}
             {success && <p className='text-green-500'>OTP verified successfully!</p>}
-
             <span className='text-[16px] text-gradient cursor-pointer' onClick={handleSubmit}>
               Resend code
             </span>
