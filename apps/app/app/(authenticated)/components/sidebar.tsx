ad45c1a486839b667f993271e44f865564c20c51
chizobavictory <chizoba.victory1@gmail.com>
2024-11-28 07:28:31 +0100

    added profect pages

--- apps/app/app/(authenticated)/components/sidebar.tsx
+++ apps/app/app/(authenticated)/components/sidebar.tsx
@@ -27,24 +27,24 @@ const GlobalSidebar: React.FC<GlobalSidebarProps> = ({ children }) => {
     { name: 'Overview', to: '/', icon: <DashboardIcon /> },
     { name: 'Projects', to: '/projects', icon: <FileTextIcon /> },
     { name: 'User Management', to: '/user-mgt', icon: <PersonIcon /> },
-    { name: 'Audit Logs', to: '/audit-logs', icon: <ListBulletIcon /> },
+    { name: 'Audit Logs', to: '/', icon: <ListBulletIcon /> },
     { name: 'Analytics', to: '/analytics', icon: <BarChartIcon /> },
     { name: 'Access Token', to: '/access-token', icon: <Crosshair2Icon /> },
     { name: 'Rewards', to: '/rewards', icon: <BookmarkFilledIcon /> },
     { name: 'Data Backup', to: '/data-backup', icon: <CloudUpload size={16} /> },
-    { name: 'Settings', to: '/', icon: <GearIcon /> },
+    { name: 'Settings', to: '/settings', icon: <GearIcon /> },
   ];
 
   return (
-    <div className="bg-blackoff w-[18%] border-r-2 border-r-[#1B1C1F] fixed hidden lg:flex flex-col items-center justify-start p-10 py-20 min-h-screen h-full">
-      <div className="flex flex-col justify-between h-full">
+    <div className='bg-blackoff w-[18%] border-r-2 border-r-[#1B1C1F] fixed hidden lg:flex flex-col items-center justify-start p-10 py-20 min-h-screen h-full overflow-y-auto'>
+      <div className='flex flex-col justify-between h-full w-full'>
         {/* Logo Section */}
         <div>
-          <Link href="/" className="font-satoshi_medium text-gradient text-2xl pl-10">
+          <Link href='/' className='font-satoshi_medium text-gradient text-2xl pl-10'>
             quikDB
           </Link>
           {/* Navigation Links */}
-          <div className="flex flex-col gap-2 mt-16">
+          <div className='flex flex-col gap-2 mt-16'>
             {navigation.map((item) => (
               <Link
                 key={item.name}
@@ -59,22 +59,24 @@ const GlobalSidebar: React.FC<GlobalSidebarProps> = ({ children }) => {
             ))}
           </div>
         </div>
-        {/* Support and Logout */}
-        <div className="py-6 mt-20 flex flex-col gap-2">
-          <Link href="/support">
-            <Button size="lg">
+        {/* Support and Logout Section */}
+        <div className='py-6 mt-6 flex flex-col gap-2 w-full'>
+          {/* Support Button */}
+          <Link href='/support'>
+            <Button size='lg' className='flex items-center gap-3 rounded-lg py-2 px-8 text-sm leading-7  hover:bg-gradient bg-blackoff text-gradient'>
               <HeadphonesIcon /> Support
             </Button>
           </Link>
-          <Link href="/logout">
-            <Button size="lg">
+          {/* Logout Button */}
+          <Link href='/logout'>
+            <Button size='lg' className='flex items-center gap-3 rounded-lg py-2 px-8 text-sm leading-7 hover:bg-gradient bg-blackoff text-gradient'>
               <LogOutIcon /> Logout
             </Button>
           </Link>
         </div>
       </div>
-      {/* Children */}
-      <div className="flex-grow">{children}</div>
+      {/* Children (Main Content) */}
+      <div className='flex-grow'>{children}</div>
     </div>
   );
 };
