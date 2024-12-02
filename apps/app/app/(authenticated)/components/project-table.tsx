d12a71e8be5cc13156d3dc430be05fe076e48597
chizobavictory <chizoba.victory1@gmail.com>
2024-12-02 07:29:52 +0100

    completed project integrations

--- apps/app/app/(authenticated)/components/project-table.tsx
+++ apps/app/app/(authenticated)/components/project-table.tsx
@@ -64,12 +64,13 @@ export function ProjectTable({ projects }: ProjectTableProps) {
   return (
     <div className='w-full'>
       <div className='flex items-center pt-7 pb-5'>
-        <Input
-          placeholder='Search by Project name...'
-          value={(table.getColumn('project')?.getFilterValue() as string) ?? ''}
-          onChange={(event) => table.getColumn('project')?.setFilterValue(event.target.value)}
-          className='max-w-sm h-11'
-        />
+      <Input
+  placeholder='Search by Project name...'
+  value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+  onChange={(event) => table.getColumn('name')?.setFilterValue(event.target.value)}
+  className='max-w-sm h-11'
+/>
+
         <DropdownMenu>
           <DropdownMenuTrigger asChild className='h-11'>
             <Button className='ml-auto'>
