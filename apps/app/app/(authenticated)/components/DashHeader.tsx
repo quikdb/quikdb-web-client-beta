e40e092da08fd2d424ea5c233967a1ce01b57f7f
chizobavictory <chizoba.victory1@gmail.com>
2024-12-04 19:28:30 +0100

    Merge branch 'victory'

--- apps/app/app/(authenticated)/components/DashHeader.tsx
+++ apps/app/app/(authenticated)/components/DashHeader.tsx
@@ -34,20 +34,21 @@ const DashHeader = () => {
           </Link>
         </DropdownMenuContent>
       </DropdownMenu>
-      <div className="flex items-center gap-3 text-lg max-md:ml-5">
-        <div className="flex gap-2">
-          <img src="/images/gem.png" alt="gem" className="w-5 object-contain max-md:w-3" />
+      <div className='flex items-center gap-3 text-lg max-md:ml-5'>
+        <div className='flex gap-2'>
+          <img src='/images/gem.png' alt='gem' className='w-5 object-contain max-md:w-3' />
           <p className='max-md:text-xs'>30</p>
         </div>
-        <hr className="border-[#1B1C1F] rotate-90 w-5 max-md:hidden" />
-        <div className="flex gap-3 items-center">
+        <hr className='border-[#1B1C1F] rotate-90 w-5 max-md:hidden' />
+        <div className='flex gap-3 items-center'>
           <NotifModal />
-          <div className="flex gap-3 max-md:gap-0">
-            <img src="/images/user.png" alt="user" className='object-contain' />
-            <p className="flex items-center gap-2 text-gray-400 max-md:text-xs">Oluwatimileyin<ChevronDown size={16} /></p>
+          <div className='flex gap-3 max-md:gap-0'>
+            <img src='/images/user.png' alt='user' className='object-contain' />
+            <p className='flex items-center gap-2 text-gray-400 max-md:text-xs'>
+              {firstName} <ChevronDown size={16} />
+            </p>
           </div>
         </div>
-
       </div>
     </div>
   );
