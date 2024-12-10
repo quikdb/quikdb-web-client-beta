39ab095eae8df141721a0051a0c6b6c65fbb1608
chizobavictory <chizoba.victory1@gmail.com>
2024-12-10 16:59:03 +0100

    get schema

--- apps/app/app/(authenticated)/components/Schema.tsx
+++ apps/app/app/(authenticated)/components/Schema.tsx
@@ -92,18 +92,19 @@ const Schema = () => {
           {schemas && schemas.length > 0 ? (
             schemas.map((schemaName: string, index: number) => (
               <AccordionItem key={index} value={`item-${index}`} className='w-full'>
-                <AccordionTrigger>
-                  <div className='flex items-center justify-between lg:w-[14vw] max-md:gap-52 w-fit'>
-                    <p className='text-base'>{schemaName}</p>
-                    <EllipsisVertical size={16} className='text-gray- max-md:' />
-                  </div>
-                </AccordionTrigger>
-                <AccordionContent className='w-full flex flex-col gap-3 pl-10'>
-                  <button className='text-sm text-blue-500 underline' onClick={() => fetchSchemaData(schemaName)}>
-                    View Full Schema
-                  </button>
-                </AccordionContent>
-              </AccordionItem>
+              <AccordionTrigger>
+                <div className='flex items-center justify-between lg:w-[14vw] max-md:gap-52 w-fit'>
+                  <p className='text-base'>{schemaName.charAt(0).toUpperCase() + schemaName.slice(1)}</p>
+                  <EllipsisVertical size={16} className='text-gray- max-md:' />
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className='w-full flex flex-col gap-3 pl-10'>
+                <button className='text-sm text-blue-500 underline' onClick={() => fetchSchemaData(schemaName)}>
+                  View Full Schema
+                </button>
+              </AccordionContent>
+            </AccordionItem>
+            
             ))
           ) : (
             <p>No schemas found</p>
