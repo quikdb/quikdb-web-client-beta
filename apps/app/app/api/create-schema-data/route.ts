5beb87b891568b2c6bf3838cab643f44d8449ccc
chizobavictory <chizoba.victory1@gmail.com>
2024-12-11 06:45:54 +0100

    get schema

--- apps/app/app/api/create-schema-data/route.ts
+++ apps/app/app/api/create-schema-data/route.ts
@@ -1,38 +1,27 @@
 import { HttpAgent, Actor } from '@dfinity/agent';
 import { idlFactory as quikdb_idl } from '../icp-database/declaration/database';
+import { v4 as uuidv4 } from 'uuid';
 
 const canisterId = 'bkyz2-fmaaa-aaaaa-qaaaq-cai';
-
 const agent = new HttpAgent({ host: 'http://127.0.0.1:4943' });
 
 const quikDB = Actor.createActor(quikdb_idl, { agent, canisterId });
 
 agent.fetchRootKey();
 
-interface GetSchemaResponse {
-  schemaName: string;
-  createdAt: bigint;
-  fields: { name: string; type: string }[];
-  indexes: string[];
-}
-
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { schemaName } = body;
+    const { schemaName, record } = body;
+    console.log('qikDB:', quikDB);
 
-    const response = (await quikDB.getSchema(schemaName)) as GetSchemaResponse[];
+    const response = await quikDB.insertData({ schemaName, record });
 
+    console.log('Response from insertData:', response);
 
-    const serializedResponse = response.map((schema) => ({
-      ...schema,
-      createdAt: schema.createdAt.toString(),
-    }));
-
-    return new Response(JSON.stringify(serializedResponse), { status: 200 });
+    return new Response(JSON.stringify({ success: true, data: response }), { status: 200 });
   } catch (error) {
-    console.error('Error in /api/get-schema:', error);
-    return new Response(JSON.stringify({ error: 'Internal server error' }), { status: 500 });
+    console.error('Error in /api/create-schema-data', error);
+    return new Response(JSON.stringify({ success: false, error: 'Failed to insert data' }), { status: 500 });
   }
 }
-
