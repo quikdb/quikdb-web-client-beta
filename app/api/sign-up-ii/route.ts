0056f5cc7329c42cd62d65046cc3061b4ccfe609
chizobavictory <chizoba.victory1@gmail.com>
2024-12-17 07:13:39 +0100

    added delet project and delet project-tokens

---
+++ app/api/sign-up-ii/route.ts
@@ -0,0 +1,25 @@
+import { cookies } from 'next/headers';
+import { CryptoUtils } from '@quikdb/design-system/lib/cryptoUtils';
+
+export async function GET(req: Request) {
+  const body = await req.json();
+  const { principalId } = body;
+
+  try {
+    const encryptedData = CryptoUtils.aesEncrypt(JSON.stringify({ principalId }), 'mysecurekey1234567890', 'uniqueiv12345678');
+
+    const response = await fetch('https://quikdb-core-beta.onrender.com/a/signinWithII', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ data: encryptedData }),
+    });
+
+    const result = await response.json();
+    console.log('signinWithIIresult:', result);
+
+    return new Response(JSON.stringify(result), { status: response.status });
+  } catch (error) {
+    console.error('Error in /api/sign-up:', error);
+    return new Response(JSON.stringify({ error: 'Internal server error' }), { status: 500 });
+  }
+}
