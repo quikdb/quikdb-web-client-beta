bff76b60205476a89e4c0d7e6460b33bf1265a70
chizobavictory <chizoba.victory1@gmail.com>
2024-12-20 23:15:58 +0100

    auth flow

---
+++ app/(authenticated)/components/ScrolingAlert.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+const ScrollingAlert: React.FC = () => {
+  return (
+    <div className="w-full overflow-hidden bg-orange-300 text-black py-2">
+      <div className="inline-block whitespace-nowrap animate-scroll tracking-widest">
+        T H I S &nbsp; A P P &nbsp; I S &nbsp; I N &nbsp; B E T A &nbsp; M O D E
+      </div>
+    </div>
+  );
+};
+
+export default ScrollingAlert;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,11 +104,17 @@ const config: Config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'scroll': {
+          '0%': { transform: 'translateX(500%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'scroll': 'scroll 10s linear infinite',
       },
+      
       typography: typographyConfig,
     },
   },
