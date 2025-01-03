1be0f60f93b3d88d13f317dee73654c3b7715a34
chizobavictory <chizoba.victory1@gmail.com>
2025-01-03 09:03:22 +0100

    remove Windows download button from the download page

--- app/(unauthenticated)/download/page.tsx
+++ app/(unauthenticated)/download/page.tsx
@@ -32,11 +32,9 @@ const DownloadPage = () => {
               Download for Mac
             </Button>
           </Link>
-          <Link href='/download-desktop'>
             <Button className='rounded-full bg-gradient-to-r from-indigo-500 to-purple-600 text-white py-3 px-8 text-lg focus:scale-90' disabled>
               Download for Windows
             </Button>
-          </Link>
         </div>
       </main>
 
